85be37df4108af2659ff85df47e23bff7331242c
Connor Stack <cstack@umich.edu>
2014-03-21 14:52:29 -0400

    Initial commit.
    Kirby on a box moving back and forth.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+	public Transform poi;
+	public float u = 0.4f;
+	public float distanceFromEdge = 7.95f;
+	public float levelWidth = 63.7f;
+	public float y = 25.6f;
+	public float z = -9f;
+
+	// Use this for initialization
+	void Start () {
+		poi = GameObject.Find ("Kirby").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float target = poi.position.x;
+
+		// Don't go past edge of level
+		if (target < distanceFromEdge) {
+			target = distanceFromEdge;
+		} if (target > (levelWidth - distanceFromEdge)) {
+			target = (levelWidth - distanceFromEdge);
+		}
+
+		float currentX = transform.position.x;
+		float newX = (1 - u) * currentX + u * target;
+		transform.position = new Vector3 (newX, y, z);
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	public float maxSpeed = 5f;
+
+	private float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		speed = Input.GetAxis ("Horizontal") * maxSpeed;
+		rigidbody2D.velocity = new Vector2 (speed, 0);
+	}
+}
