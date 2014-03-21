2e91d2d2443eafa15663dad20564df4bcce18465
smyrick <smyrick@umich.edu>
2014-03-21 19:05:03 -0400

    Water damage

---
+++ UnityProject/Assets/Scripts/Enviroment/EnviromentDamage.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnviromentDamage : MonoBehaviour {
+
+	public float damageAmount = 0.5f;
+
+	void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log("Player enter");
+		if (other.gameObject.tag == "Player") {
+			other.gameObject.SendMessage("TakeDamage", damageAmount);
+		}
+	}
+
+}
---
+++ UnityProject/Assets/Scripts/Player/CharacterHealth.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterHealth : MonoBehaviour {
+
+	public float hp = 1f;
+	public float damageAmount = 1.0f;
+	public bool dead = false;
+
+	void Start () {
+	}
+	
+	void Update() {
+	}
+	
+	public bool TakeDamage(float damage) {
+		hp -= damage;
+		if (hp <= 0f) {
+			Die();
+			return true;
+		} else
+			return false;
+	}
+	
+	void Die() {
+		dead = true;
+		Debug.Log("Character Dead");
+	}
+}
