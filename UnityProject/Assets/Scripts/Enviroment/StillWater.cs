ad2fe52cfcb0c3931de0d29cc55100a2901561ea
Jacob Hash <jhash@umich.edu>
2014-03-25 20:18:48 -0400

    Moving Water - New Scene to Avoid Conflicts

---
+++ UnityProject/Assets/Scripts/Enviroment/StillWater.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StillWater : MonoBehaviour {
+
+	public Texture2D[] frames = null;
+	public float framesPerSecond = 10.0f;
+
+	void Update () {
+		if (frames != null) {
+			int index = (int) (Time.time * framesPerSecond);
+			index = index % frames.Length;
+			renderer.material.mainTexture = frames[index];
+		}
+	}
+
+}
