8fe419ae2497c57389d8aa00ec774c203f35e8e1
Mingrui Jiang <mjray369@gmail.com>
2014-03-26 14:31:24 -0400

    Enemy script DeathBonus.cs, attach to enemy to give player bonus after killing

---
+++ UnityProject/Assets/Scripts/Character/Enemy/DeathBonus.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Mingrui
+// just attach this script to any enemy, the OnDestroy function does all the work
+
+public class DeathBonus : MonoBehaviour {
+    public int bonus_seconds;
+    
+    private Player poi;
+    private TimerControl timer_control;
+
+    void Start() {
+        poi = (Player)GameObject.Find("Player").GetComponent<Player>();
+        timer_control = poi.GetComponent<TimerControl>();
+    }
+
+    void OnDestroy() {
+        Time_Bonus(bonus_seconds);
+    }
+
+    private void Time_Bonus(int extra_seconds) {
+        timer_control.increase_time(extra_seconds);
+    }
+}
--- UnityProject/Assets/Scripts/Character/Player/PlayerHealth.cs
+++ UnityProject/Assets/Scripts/Character/Player/PlayerHealth.cs
@@ -1,123 +1,1 @@
-using UnityEngine;
-using System.Collections;
-
-public class PlayerHealth : MonoBehaviour {
-	public int currentHealth;
-	public int maxHealth;
-	public int currentBreath;
-	public int maxBreath;
-	private bool inWater;
-	public float breathPercent = 1f;
-	public int current_life_count;
-	public bool invulnerable;
-	public GameObject spawnPoint;
-
-	private Player poi;
-	private TimerControl timer_control;
-	private LungCapacityAbility lungs;
-
-	void Start() {
-		poi = (Player)GameObject.Find("Player").GetComponent<Player>();
-		timer_control = poi.GetComponent<TimerControl>();
-	}
-
-	public void startDrowning() {
-		inWater = true;
-	}
-
-	public void stopDrowning() {
-		inWater = false;
-		currentBreath = maxBreath;
-		breathPercent = (float) currentBreath / maxBreath;
-	}
-
-	public bool isDrowning() {
-		return inWater;
-	}
-	
-	public void decreaseBreath(int amount) {
-		if(currentBreath <= 0) {
-			decreaseHealth (amount, lungs);
-		} else {
-			currentBreath -= amount;
-			breathPercent = (float) currentBreath / maxBreath;
-		}
-	}
-	
-	public void resetHealth() {
-		currentHealth = maxHealth;
-	}
-
-	public void increaseHealth(int amount) {
-		currentHealth += amount;
-		checkPossible();
-	}
-	
-	public void decreaseHealth(int amount, Ability ability) {
-		if(invulnerable){return;}
-		currentHealth -= amount;
-		checkAlive(ability);
-	}
-	
-	public void increaseMaxHealth(int amount) {
-		maxHealth += amount;
-	}
-	
-	public void increaseLifeCount(int amount){
-		current_life_count += amount;
-	}
-	
-	private void checkAlive(Ability ability) { 
-		if(currentHealth <= 0){
-			playerDeath(ability);
-		}
-	}
-	
-	private void checkPossible() { 
-		if(currentHealth > maxHealth){
-			currentHealth = maxHealth;
-		}
-	}
-	
-	// check if perma or temp death
-	public void playerDeath(Ability ability) {
-		--current_life_count;
-		resetPlayer ();
-		if(current_life_count <= 0){
-			permaDeath();
-		}
-		else
-		{
-			tempDeath(ability);
-		}
-	}
-	
-    // Time is RESET here
-	private void permaDeath()
-	{
-        timer_control.restartTimer();
-		Application.LoadLevel ("Main");
-	}
-
-    // This function does NOT reset time
-	private void resetPlayer() {
-		currentHealth = maxHealth;
-		poi.transform.position = spawnPoint.transform.position;
-		if (poi.feetInWater) {
-			poi.feetExitWater();
-		}
-		if (poi.headUnderwater) {
-			poi.headExitWater();
-		}
-	}
-	
-	private void tempDeath(Ability ability)
-	{
-		//Prints ability that's being added
-		Debug.Log ("Died and gained the ability " + ability.abilityName);
-	
-		if (ability != null) {
-			poi.AddAbility(ability);
-		}
-	}
-}
+using UnityEngine;using System.Collections;public class PlayerHealth : MonoBehaviour {	public int currentHealth;	public int maxHealth;	public int currentBreath;	public int maxBreath;	private bool inWater;	public float breathPercent = 1f;	public int current_life_count;	public bool invulnerable;	public GameObject spawnPoint;	private Player poi;	private TimerControl timer_control;	private LungCapacityAbility lungs;	void Start() {		poi = (Player)GameObject.Find("Player").GetComponent<Player>();		timer_control = poi.GetComponent<TimerControl>();	}	public void startDrowning() {		inWater = true;	}	public void stopDrowning() {		inWater = false;		currentBreath = maxBreath;		breathPercent = (float) currentBreath / maxBreath;	}	public bool isDrowning() {		return inWater;	}		public void decreaseBreath(int amount) {		if(currentBreath <= 0) {			decreaseHealth (amount, lungs);		} else {			currentBreath -= amount;			breathPercent = (float) currentBreath / maxBreath;		}	}		public void resetHealth() {		currentHealth = maxHealth;	}	public void increaseHealth(int amount) {		currentHealth += amount;		checkPossible();	}		public void decreaseHealth(int amount, Ability ability) {		if(invulnerable){return;}		currentHealth -= amount;		checkAlive(ability);	}		public void increaseMaxHealth(int amount) {		maxHealth += amount;	}		public void increaseLifeCount(int amount){		current_life_count += amount;	}		private void checkAlive(Ability ability) { 		if(currentHealth <= 0){			playerDeath(ability);		}	}		private void checkPossible() { 		if(currentHealth > maxHealth){			currentHealth = maxHealth;		}	}		// check if perma or temp death	public void playerDeath(Ability ability) {		--current_life_count;		resetPlayer ();		if(current_life_count <= 0){			permaDeath();		}		else		{			tempDeath(ability);		}	}	    // Time is RESET here	private void permaDeath()	{        timer_control.restartTimer();		Application.LoadLevel ("Main");	}    // This function does NOT reset time	private void resetPlayer() {		currentHealth = maxHealth;		poi.transform.position = spawnPoint.transform.position;		if (poi.feetInWater) {			poi.feetExitWater();		}		if (poi.headUnderwater) {			poi.headExitWater();		}	}		private void tempDeath(Ability ability)	{		//Prints ability that's being added		Debug.Log ("Died and gained the ability " + ability.abilityName);			if (ability != null) {			poi.AddAbility(ability);		}	}}
