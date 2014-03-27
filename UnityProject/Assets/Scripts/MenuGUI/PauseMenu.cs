f9814fdfdfcdeb27dceea98e714439f48e56e758
Mingrui Jiang <mjray369@gmail.com>
2014-03-27 13:34:35 -0400

    Pause Menu, GUI texture folder organization

---
+++ UnityProject/Assets/Scripts/MenuGUI/PauseMenu.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+
+    public GUITexture pause_GUI;
+
+    private bool is_paused =false;
+
+    void Update () {
+        detect_pause();
+	}
+
+    void detect_pause() {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(is_paused) {
+                is_paused = false;
+            }
+            else {
+                is_paused = true;
+            }
+        }
+
+        if(is_paused) {
+            Time.timeScale = 0;
+            pause_GUI.enabled = true;
+        }
+        else
+        {
+            Time.timeScale = 1;
+            pause_GUI.enabled = false;
+        }
+    }
+}
