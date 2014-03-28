7ac81fecee8dd4f2c73783ccf80b7a7feb26844d
Connor Stack <cstack@umich.edu>
2014-03-28 00:05:34 -0400

    Melee Attack script.

---
+++ UnityProject/Assets/Scripts/Ability/ActiveAbility/BasicAttack.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicAttack : ActiveAbility {
+	public GameObject attackColliderPrefab;
+
+	protected override void Awake() {
+		abilityName = "Melee";
+	}
+	
+	protected override IEnumerator OnActivate () {
+		GameObject collider = Instantiate (attackColliderPrefab) as GameObject;
+		collider.transform.position = character.transform.position;
+		if (character.dir == EntityBase.Direction.Left) {
+			collider.transform.position += new Vector3(-1f, 0f, 0f);
+		} else {
+			collider.transform.position += new Vector3(1f, 0f, 0f);
+		}
+		character.GetComponent<Animator> ().SetInteger ("ability", (int) GlobalConstant.AbilityAnimation.MeleeAttack);
+	}
+}
