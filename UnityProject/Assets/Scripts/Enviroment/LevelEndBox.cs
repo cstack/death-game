978030036b4c159aa6c1c5a2254e5c260eaa3c29
Jacob Hash <jhash@umich.edu>
2014-04-03 12:30:39 -0400

    GameInfo Passing Between Levels Beautifully

---
+++ UnityProject/Assets/Scripts/Enviroment/LevelEndBox.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelEndBox : MonoBehaviour {
+	public GameInfo ginfo;
+	public GameObject gobject;
+
+	void Start () {
+		GameObject tmp = GameObject.Find ("GameInfo");
+
+		if (tmp == null) {
+			tmp = GameObject.Instantiate(gobject) as GameObject;
+			tmp.name = "GameInfo";
+		} 
+
+		ginfo = tmp.GetComponent<GameInfo> ();
+	}
+
+	void OnTriggerEnter2D (Collider2D col) {
+		if (col.gameObject.tag == "Player") {
+			if (ginfo != null) {
+				Application.LoadLevel (ginfo.nextLevel());
+			} else {
+				Debug.Log ("No GameInfo GameObject Found, Restarting Current Level");
+				Application.LoadLevel (Application.loadedLevel);
+			}
+		}
+	}
+}
--- UnityProject/Assets/Scripts/Utility/GameInfo.cs
+++ UnityProject/Assets/Scripts/Utility/GameInfo.cs
@@ -1,9 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 //Used to Pass Information Between Scenes (and To Server? - Heat Maps)
 
 public class GameInfo : MonoBehaviour {
 
+	private int curLevel = 0;
 
+	public List<string> levelStrings = new List<string>()
+	{
+		"Jake",
+		"Jake2"
+	};
+
+	void Awake() {
+		DontDestroyOnLoad(transform.gameObject);
+	}
+
+	public string nextLevel () {
+
+		if (curLevel + 1 < levelStrings.Count) {
+			return levelStrings[++curLevel];
+		}
+
+		return levelStrings [0];
+	}
+	
 }
