6bb21796ab9c08115b71801aad7e16e45b6d5e12
Mingrui Jiang <mjray369@gmail.com>
2014-04-05 21:02:16 -0400

    Main Menu Added. It is a new scene
    Team. Description. Control. Play Button

---
+++ UnityProject/Assets/Scripts/Utility/Menu.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+    // mingrui
+    // these will be even handlers
+    // signals are sent from buttons using NGUI's native api
+    public void OnPlayClicked() {
+        //Debug.Log("Play button clicked");
+
+        Application.LoadLevel("Main");
+    }
+}
