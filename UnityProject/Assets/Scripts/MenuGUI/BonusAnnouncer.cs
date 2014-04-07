9610c58894c4282fc052b7f74d499708de3157fc
Mingrui Jiang <mjray369@gmail.com>
2014-04-06 21:18:47 -0400

    Time Bonus Announcer in Mingrui's scene

---
+++ UnityProject/Assets/Scripts/MenuGUI/BonusAnnouncer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BonusAnnouncer : MonoBehaviour {
+
+    public UILabel bonus_label;
+
+    private float msg_duration = 1;
+    private float timer = 0;
+
+    void Start() {
+        bonus_label.enabled = false;
+    }
+
+	// Update is called once per frame
+	void Update () {
+	    // period the bonus message is shown
+        if(bonus_label.enabled){
+            timer += Time.deltaTime;
+            if(timer > msg_duration){
+                bonus_label.enabled = false;
+                timer = 0;
+            }
+        }
+	}
+
+    public void Announce_Bonus(string msg) {
+        bonus_label.text = "+ " + msg;
+        bonus_label.enabled = true;
+    }
+}
