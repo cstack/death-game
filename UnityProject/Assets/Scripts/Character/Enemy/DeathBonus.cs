0fffab69af18b8b1b4d4d40eeca70f16348747ec
Mingrui Jiang <mjray369@gmail.com>
2014-04-06 21:18:47 -0400

    Time Bonus Announcer in Mingrui's scene

--- UnityProject/Assets/Scripts/Character/Enemy/DeathBonus.cs
+++ UnityProject/Assets/Scripts/Character/Enemy/DeathBonus.cs
@@ -6,13 +6,16 @@ using System.Collections;
 
 public class DeathBonus : MonoBehaviour {
     public int bonus_seconds;
-    
+
+    private BonusAnnouncer bonus_announcer;
+
     private Player poi;
     private TimerControl timer_control;
 
     void Start() {
         poi = (Player)GameObject.Find("Player").GetComponent<Player>();
         timer_control = poi.GetComponent<TimerControl>();
+        bonus_announcer = (BonusAnnouncer)GameObject.Find("Bonus Announcer").GetComponent<BonusAnnouncer>();
     }
 
     void OnDestroy() {
@@ -21,5 +24,8 @@ public class DeathBonus : MonoBehaviour {
 
     private void Time_Bonus(int extra_seconds) {
         timer_control.increase_time(extra_seconds);
+        // tell bonus announcer to announce the time bonus for player to feel better
+        // about themselves
+        bonus_announcer.Announce_Bonus(string.Format("{0:00}:{1:00}", extra_seconds / 60, extra_seconds % 60));
     }
 }
