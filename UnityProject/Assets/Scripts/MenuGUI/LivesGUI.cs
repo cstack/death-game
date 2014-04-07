d676b1701daaee15a5d14b873ee4f488fe66eece
Mingrui Jiang <mjray369@gmail.com>
2014-04-06 20:36:08 -0400

    changed timer and lives GUI, change mainmenu game description

---
+++ UnityProject/Assets/Scripts/MenuGUI/LivesGUI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LivesGUI : MonoBehaviour {
+    public UILabel lives_label;
+
+    private Transform poi;
+    private PlayerHealth player_health;
+
+    void Start()
+    {
+        poi = GameObject.Find("Player").transform;
+        player_health = poi.GetComponent<PlayerHealth>();
+    }
+
+    void Update()
+    {
+        lives_label.text = "X " + player_health.current_life_count;
+    }
+}
---
+++ UnityProject/Assets/Scripts/MenuGUI/TimerGUI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimerGUI : MonoBehaviour {
+
+    public UILabel timer_label;
+
+    private Transform poi;
+    private TimerControl timer_control;
+
+    void Start() {
+        poi = GameObject.Find("Player").transform;
+        timer_control = poi.GetComponent<TimerControl>();
+    }
+
+    void Update() {
+        int seconds = (int)timer_control.read_time();
+        timer_label.text = string.Format("{0:00}:{1:00}", seconds / 60, seconds % 60);
+    }
+}
