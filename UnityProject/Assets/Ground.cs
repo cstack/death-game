67bb83e624daae09b9eb13476a76b6289481d27a
Jacob Hash <jhash@umich.edu>
2014-04-07 20:41:31 -0400

    Ground Textures, Woohoo!

---
+++ UnityProject/Assets/Ground.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ground : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		renderer.material.SetTextureScale ("_MainTex", new Vector2(transform.localScale.x, 1f));
+
+	}
+}
