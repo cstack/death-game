50c1bec8465bb418f6e723f6d2a1cf9d0713cc98
Jacob Hash <jhash@umich.edu>
2014-04-09 08:23:20 -0400

    Jumping, Animating, and Crouching, Oh my!

--- UnityProject/Assets/Scripts/Character/Player/Player.cs
+++ UnityProject/Assets/Scripts/Character/Player/Player.cs
@@ -8,10 +8,13 @@ public class Player : CharacterBase {
 	public bool grounded;
 	public bool feetInWater;
 	public bool headUnderwater;
+	public bool crouching = false;
+	public bool animating = false;
 	public float jumpSpeed = 5f;
 	public float swimSpeed = 2f;
 	public float waterDrag = 4f;
 	public float waterGravity = 0.5f;
+	public float jumptimer = 0.1f;
 
 	public PlayerHealth playerHealth;
 	public float speed;
@@ -35,8 +38,13 @@ public class Player : CharacterBase {
 	private void Update () {
 		Get_Aim(); // mingrui
 
-		HorizontalMove ();
-		VerticalMove ();
+		if (!crouching) {
+			HorizontalMove ();
+		}
+
+		if (!ability_control.animating) {
+			VerticalMove ();
+		}
 
 	}
 
@@ -83,21 +91,31 @@ public class Player : CharacterBase {
 				animator.SetBool("grounded", false);
 				
 				//Let animation finish first!
-				Invoke("Jump", 0.1f);
+				Invoke("Jump", jumptimer);
 			}
 		}
 
-		if (Input.GetButtonUp("Jump") && rigidbody2D.velocity.y > 0 && !feetInWater) {
+		if (!Input.GetButton("Jump") && rigidbody2D.velocity.y > 0 && !feetInWater) {
+			animator.SetTrigger("exitjump");
 			updateYVelocity(0);
 		}
 
-		if (Input.GetAxis("Vertical") < 0f && grounded && !feetInWater) {
+		if (Input.GetButton("Downward") && grounded && !feetInWater) {
+			crouching = true;
+			animator.SetBool("crouching", crouching);
+		}
 
+		if (!Input.GetButton("Downward")) {
+			crouching = false;
+			animator.SetBool("crouching", crouching);
 		}
 
 	}
 
 	private void Jump() {
+		if (!Input.GetButton("Jump")) {
+			return;
+		}
 
 		updateYVelocity(jumpSpeed);
 		grounded = false;
@@ -149,12 +167,15 @@ public class Player : CharacterBase {
 	}
 
 	public void AbilityAnimationHit() {
+
 		if (ability_control.current_ability != null) {
 			ability_control.current_ability.Hit ();
 		}
 	}
 
 	public void AbilityAnimationFinished() {
+		ability_control.animating = false;
+
 		if (ability_control.current_ability != null) {
 			ability_control.current_ability.Finish ();
 			ability_control.current_ability = null;
