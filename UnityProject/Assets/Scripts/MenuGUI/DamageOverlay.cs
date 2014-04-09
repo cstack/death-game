1a10de5c20caad2e9a59004d5ef504eca99be6c2
Connor Stack <cstack@umich.edu>
2014-04-09 18:03:42 -0400

    Screen turns red when you get hurt.

---
+++ UnityProject/Assets/Scripts/MenuGUI/DamageOverlay.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageOverlay : MonoBehaviour {
+	private UITexture overlay;
+	private PlayerHealth player_health;
+	
+	void Start()
+	{
+		GameObject player = GameObject.Find("Player");
+		player_health = player.GetComponent<PlayerHealth>();
+		overlay = GetComponent<UITexture> ();
+	}
+	
+	void Update()
+	{
+		overlay.alpha = 1 - (player_health.currentHealth / player_health.maxHealth);
+	}
+}
