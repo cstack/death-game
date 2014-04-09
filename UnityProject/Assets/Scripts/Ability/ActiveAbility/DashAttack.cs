83da65918d25de9d8bb82bdca54f2bb65dea00a2
Connor Stack <cstack@umich.edu>
2014-04-08 23:53:08 -0400

    Dash Attack.

---
+++ UnityProject/Assets/Scripts/Ability/ActiveAbility/DashAttack.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DashAttack : ActiveAbility {
+
+	protected override void Awake() {
+		abilityName = "Dash Attack";
+	}
+	
+	protected override void OnActivate () {
+		player.startDash ();
+	}
+
+}
