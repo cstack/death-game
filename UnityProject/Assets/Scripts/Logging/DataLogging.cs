0729614d90a0d1451470d3d08326e2783d7e82d4
smyrick <smyrick@umich.edu>
2014-04-09 16:35:51 -0400

    Parse Imports

---
+++ UnityProject/Assets/Scripts/Logging/DataLogging.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Parse;
+
+public class DataLogging : MonoBehaviour {
+	
+	void Start () {
+		ParseObject.RegisterSubclass<DeathEvent>();
+	}
+
+	void Update () {
+	
+	}
+}
---
+++ UnityProject/Assets/Scripts/Logging/DeathEvent.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Parse;
+
+[ParseClassName("DeathEvent")]
+public class DeathEvent : ParseObject {
+
+	[ParseFieldName("WorldPosition")]
+	public string WorldPosition
+	{
+		get { return GetProperty<string>("WorldPosition"); }
+		set { SetProperty<string>(value, "WorldPosition"); }
+	}
+
+	[ParseFieldName("LifeTime")]
+	public int LifeTime
+	{
+		get { return GetProperty<int>("LifeTime"); }
+		set { SetProperty<int>(value, "LifeTime"); }
+	}
+}
