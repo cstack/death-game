a796c091522d8b9bc4f710c882cc41b3f1c29e9a
Connor Stack <cstack@umich.edu>
2014-04-09 01:42:52 -0400

    Kill droplets after time.

---
+++ UnityProject/Assets/DieAfterTime.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DieAfterTime : MonoBehaviour {
+	public float time;
+	private float startTime;
+
+	// Use this for initialization
+	void Start () {
+		startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time - startTime > time) {
+			Destroy(gameObject);
+		}
+	}
+}
