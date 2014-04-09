04292ba087b8d5a3f7129b9406e76fc1e675ff74
Mingrui Jiang <mjray369@gmail.com>
2014-04-08 21:54:48 -0400

    added ability tester

--- UnityProject/Assets/MingruiFileNotReady/AbilityTesterControl.cs
+++ UnityProject/Assets/MingruiFileNotReady/AbilityTesterControl.cs
@@ -4,10 +4,12 @@ using System.Collections;
 public class AbilityTesterControl : MonoBehaviour {
 
 	public Ability ability_to_test;
+	private int attackPower = 100000;
 
 	void OnCollisionEnter2D(Collision2D col){
 		if(col.gameObject.tag == "Player"){
-			Debug.Log("Player");
+			//Debug.Log("Player");
+			col.gameObject.GetComponent<PlayerHealth>().decreaseHealth(attackPower, ability_to_test);
 		}
 	}
 
