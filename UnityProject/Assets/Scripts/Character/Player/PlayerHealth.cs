f341c0436e178596bcde78be97ccaa8a92c3192e
smyrick <smyrick@umich.edu>
2014-04-14 15:24:31 -0400

    Uplink player deaths and abilities used to game session

--- UnityProject/Assets/Scripts/Character/Player/PlayerHealth.cs
+++ UnityProject/Assets/Scripts/Character/Player/PlayerHealth.cs
@@ -26,6 +26,7 @@ public class PlayerHealth : MonoBehaviour {
 		if (DataLogging.enabled) {
 			DataLogging.playerDeaths = new ParseObject("PlayerDeaths");
 			DataLogging.gameSession["playerDeaths"] = DataLogging.playerDeaths;
+			DataLogging.playerDeaths["gameSession"] = DataLogging.gameSession;
 			DataLogging.gameSession.SaveAsync();
 		}
 	}
