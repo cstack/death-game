9c3e7a66810ddb691c4c05ab9be3eb453d9a0445
Jacob Hash <jhash@umich.edu>
2014-04-15 15:41:03 -0400

    Small Crouch Fix

--- UnityProject/Assets/Scripts/Enviroment/AirPocket.cs
+++ UnityProject/Assets/Scripts/Enviroment/AirPocket.cs
@@ -1,9 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class AirPocket : MonoBehaviour {
-
+	
 	public EnvironmentDamage fatherWater;
+
+	public Texture2D[] frames = null;
+	public float framesPerSecond = 10.0f;
+
+	void Update () {
+		if (frames != null) {
+			int index = (int) (Time.time * framesPerSecond);
+			index = index % frames.Length;
+			renderer.material.mainTexture = frames[index];
+		}
+
+	}
 
 	void OnTriggerEnter2D (Collider2D col) {
 		if (col.gameObject.tag == "PlayerHead") {
