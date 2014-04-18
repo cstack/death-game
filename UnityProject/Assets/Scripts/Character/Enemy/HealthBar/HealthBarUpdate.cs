ea84250daf194a43654660a006ef7ff2397ac013
Connor Stack <cstack@umich.edu>
2014-04-17 22:17:28 -0400

    Health Bar for enemies.

---
+++ UnityProject/Assets/Scripts/Character/Enemy/HealthBar/HealthBarUpdate.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthBarUpdate : MonoBehaviour {
+
+	private UISlider slider;
+	private EnemyHealth enemyHealth;
+	
+	// Use this for initialization
+	void Start()
+	{
+		slider = GetComponent<UISlider>();
+		enemyHealth = transform.parent.parent.parent.gameObject.GetComponent<EnemyHealth> ();
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		slider.value = enemyHealth.getHP () / enemyHealth.maxHP;
+	}
+}
