caf2c4a31a4162535ca1bd048be8b0bbb4c4ac65
Connor Stack <cstack@umich.edu>
2014-04-21 00:39:19 -0400

    Add floating souls to final boss arena.

---
+++ UnityProject/Assets/Scripts/Enviroment/Soul.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic; // List
+
+public class Soul : MonoBehaviour {
+	public List<GameObject> waypoints;
+	public float speed = 3f;
+
+	private int waypointIndex;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (Wander ());
+	}
+
+	private IEnumerator Wander() {
+		while (waypointIndex < waypoints.Count) {
+			Vector3 offset = waypoints[waypointIndex].transform.position - transform.position;
+
+			if (offset.magnitude <= 5f) {
+				waypointIndex++;
+			}
+
+			Vector3 vel = offset.normalized * speed + new Vector3(Random.value - 0.5f, Random.value - 0.5f, 0);
+			rigidbody2D.velocity = vel;
+			yield return new WaitForSeconds(Random.value * 5f);
+		}
+
+		Destroy(gameObject);
+	}
+}
---
+++ UnityProject/Assets/Scripts/Enviroment/SoulSpawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic; // List
+
+public class SoulSpawner : MonoBehaviour {
+	public Soul soulPrefab;
+	public List<GameObject> waypoints;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (SpawnSouls ());
+	}
+	
+	private IEnumerator SpawnSouls() {
+		while (true) {
+			Soul soul = (Soul) Instantiate(soulPrefab);
+			soul.waypoints = waypoints;
+			soul.transform.position = transform.position;
+			yield return new WaitForSeconds(Random.value*3f);
+		}
+	}
+}
