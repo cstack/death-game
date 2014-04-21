bec9d8b11732d11718750a6e47f3ad17feffd311
Connor Stack <cstack@umich.edu>
2014-04-20 20:49:50 -0400

    Let player attack enemy projectiles.

--- UnityProject/Assets/Scripts/Ability/ActiveAbility/AttackArea.cs
+++ UnityProject/Assets/Scripts/Ability/ActiveAbility/AttackArea.cs
@@ -4,8 +4,10 @@ using System.Collections;
 public class AttackArea : MonoBehaviour {
 
 	void OnTriggerEnter2D (Collider2D other){
-		if(other.gameObject.tag == "Enemy"){
+		if(other.gameObject.tag == GlobalConstant.Tag.Enemy){
 			other.gameObject.GetComponent<EnemyHealth>().TakeDamage(1f);
+		} else if (other.gameObject.layer == LayerMask.NameToLayer(GlobalConstant.Layer.EnemyProjectile)) {
+			Destroy(other.gameObject);
 		}
 	}
 }
