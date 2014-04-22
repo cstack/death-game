5db7e1843ae521b81256fe527a39feb413bec69a
Connor Stack <cstack@umich.edu>
2014-04-22 14:01:39 -0400

    Fade background when you kill boss. Crazy sound when you enter light.

--- UnityProject/Assets/Scripts/Character/Enemy/EnemyDestroyer.cs
+++ UnityProject/Assets/Scripts/Character/Enemy/EnemyDestroyer.cs
@@ -3,6 +3,19 @@ using System.Collections;
 
 public class EnemyDestroyer : MonoBehaviour {
 
+	private IEnumerator Start() {
+		float initialVolume = Camera.main.audio.volume;
+		float volume = initialVolume;
+		float initialTime = Time.time;
+		float timeToFade = 5f;
+		while (Time.time - initialTime < timeToFade) {
+			float percentThroughFade = (Time.time - initialTime) / timeToFade;
+			float volumeMultiplier = 1 - percentThroughFade;
+			Camera.main.audio.volume = initialVolume * volumeMultiplier;
+			yield return null;
+		}
+	}
+
 	private void OnTriggerEnter2D (Collider2D other){
 		if(other.gameObject.tag == GlobalConstant.Tag.Enemy){
 			Destroy(other.gameObject);
