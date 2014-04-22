9b54782fafc76dff1171ac558a229e7295435b37
Connor Stack <cstack@umich.edu>
2014-04-22 12:17:37 -0400

    Boss death animation - explode into a cloud of souls.

---
+++ UnityProject/Assets/Scripts/Character/Enemy/EnemyDestroyer.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyDestroyer : MonoBehaviour {
+
+	private void OnTriggerEnter2D (Collider2D other){
+		if(other.gameObject.tag == GlobalConstant.Tag.Enemy){
+			Destroy(other.gameObject);
+		}
+	}
+}
