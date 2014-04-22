7da27481974de9f683c76a62d122026ca0fdf51f
Connor Stack <cstack@umich.edu>
2014-04-22 12:17:37 -0400

    Boss death animation - explode into a cloud of souls.

--- UnityProject/Assets/Scripts/Enviroment/Soul.cs
+++ UnityProject/Assets/Scripts/Enviroment/Soul.cs
@@ -5,11 +5,19 @@ using System.Collections.Generic; // List
 public class Soul : MonoBehaviour {
 	public List<GameObject> waypoints;
 	public float speed = 3f;
+	public bool explode;
+	public Vector3 explosionOrigin;
 
 	private int waypointIndex;
 
 	// Use this for initialization
-	void Start () {
+	private IEnumerator Start () {
+		if (explode) {
+			rigidbody2D.velocity = (transform.position - explosionOrigin).normalized * speed * 2f;
+			rigidbody2D.drag = 1f;
+			yield return new WaitForSeconds(3f + Random.value * 7f);
+			rigidbody2D.drag = 0f;
+		}
 		StartCoroutine (Wander ());
 	}
 
