a8f6c216ab73eeba5beda27fc86e1788b2ea3878
Connor Stack <cstack@umich.edu>
2014-04-22 23:06:21 -0400

    Music fade out at end of level.

---
+++ UnityProject/Assets/Scripts/Sound/EndOfLevelSound.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndOfLevelSound : MonoBehaviour {
+
+	public void FadeOut(float timeToFade) {
+		StartCoroutine (FadeOutCoroutine (timeToFade));
+	}
+
+	public IEnumerator FadeOutCoroutine(float timeToFade) {
+		float initialVolume = audio.volume;
+		float volume = initialVolume;
+		float initialTime = Time.time;
+		while (Time.time - initialTime < timeToFade) {
+			float percentThroughFade = (Time.time - initialTime) / timeToFade;
+			float volumeMultiplier = 1 - percentThroughFade;
+			audio.volume = initialVolume * volumeMultiplier;
+			yield return null;
+		}
+	}
+}
